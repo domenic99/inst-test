6ee5d99722c5d2b48a6f62b008c8674efaa1099c
domenic99 <14790265+domenic99@users.noreply.github.com>
2024-06-03 17:44:03 +0300

    Add status to job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
+  DEFAULT_JOB_STATUS = :deactivated
+  STATUS_MAPPING = {
+    'Job::Event::Activate' => :activated,
+    'Job::Event::Deactivated' => :deactivated,
+  }.freeze
+
   has_many :applications, dependent: :destroy
   has_many :job_events, class_name: 'Job::Event',  dependent: :destroy
 
@@ -6,5 +12,9 @@ class Job < ApplicationRecord
     joins(:job_events)
       .where(job_events: { id: Job::Event.select('MAX(job_events.id) as id')
                                          .group(:job_id), type: 'Job::Event::Activate' })
+  end
+
+  def status
+    STATUS_MAPPING[job_events.last&.type] || DEFAULT_JOB_STATUS
   end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe Job, type: :model do
       it { is_expected.not_to include(job) }
     end
   end
+
+  describe '#status' do
+    subject { job.status }
+
+    let(:job) { create(:job) }
+
+    context 'when last job event is activate' do
+      before { create(:job_event_activate, job: job) }
+
+      it { is_expected.to eq(:activated) }
+    end
+
+    context 'when last job event is deactivate' do
+      before { create(:job_event_deactivate, job: job) }
+
+      it { is_expected.to eq(:deactivated) }
+    end
+
+    context 'when there is no event' do
+      it { is_expected.to eq(Job::DEFAULT_JOB_STATUS) }
+    end
+  end
 end
